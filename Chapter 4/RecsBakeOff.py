65eb35e76e3d56eb4685eb0006b1be8926fd5c6f
lyamlim97 <lyamlim97@gmail.com>
2023-02-26 08:34:14 +0800

    added chapter 4 files

---
+++ Chapter 4/RecsBakeOff.py
@@ -0,0 +1,37 @@
+from MovieLens import MovieLens
+from surprise import SVD
+from surprise import NormalPredictor
+from Evaluator import Evaluator
+
+import random
+import numpy as np
+
+
+def LoadMovieLensData():
+    ml = MovieLens()
+    print("Loading movie ratings...")
+    data = ml.loadMovieLensLatestSmall()
+    print("\nComputing movie popularity ranks so we can measure novelty later...")
+    rankings = ml.getPopularityRanks()
+    return (data, rankings)
+
+
+np.random.seed(0)
+random.seed(0)
+
+# load up common data set for the recommender algorithms
+(evaluationData, rankings) = LoadMovieLensData()
+
+# construct an Evaluator object
+evaluator = Evaluator(evaluationData, rankings)
+
+# add in an SVD recommender
+SVDAlgorithm = SVD(random_state=10)
+evaluator.AddAlgorithm(SVDAlgorithm, "SVD")
+
+# add in a random recommender
+Random = NormalPredictor()
+evaluator.AddAlgorithm(Random, "Random")
+
+# compare algorithms
+evaluator.Evaluate(True)
